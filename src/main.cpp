5adbadcc9f5b2de69b6911c82915f8a1322d1c01
Ethan Marreel <ethan@marreel.com>
2021-11-29 16:58:35 -0800

    Porting Code
    Porting the code from the old repository to now use the organization for TuHS robotics

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
 #include "main.h"
+#include "replay.hpp"
+
+
+/**
+ * Disables all tasks.
+ *
+ * This runs during disabled and initialize to turn off all user created tasks.
+ */
+void
+disable_all_tasks() {
+	drive_pid.suspend();
+}
+
+void
+tare_sensors() {
+	zero_mogo();
+	zero_tilter();
+}
+
+
 
 /**
  * A callback function for LLEMU's center button.
@@ -6,7 +26,8 @@
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_center_button() {
+void
+on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
 	if (pressed) {
@@ -16,25 +37,99 @@ void on_center_button() {
 	}
 }
 
+const int num_of_pages = 3;
+int current_page = 0;
+
+void
+auto_select(bool is_auton) {
+	for (int i = 0; i<7;i++)
+		pros::lcd::clear_line(i);
+
+	pros::lcd::set_text(0, "Page "+std::to_string(current_page+1));
+
+	switch (current_page) {
+		case 0: // Auto 1
+			pros::lcd::set_text(2, "Solo Win Point");
+			pros::lcd::set_text(3, "Starting Position:");
+			pros::lcd::set_text(4, "Plat Down");
+			if (is_auton) solo_awp();
+			break;
+		case 1: // Auto 2
+			pros::lcd::set_text(2, "Example Auton 1");
+			if (is_auton) auto_3();
+			break;
+		case 2: // Auto 3
+			pros::lcd::set_text(2, "Example Auton 2");
+			if (is_auton) test_auton();
+			break;
+
+		default:
+			break;
+	}
+}
+void
+page_up() {
+	if(current_page==num_of_pages-1)
+		current_page=0;
+	else
+		current_page++;
+	auto_select(false);
+}
+void
+page_down() {
+	if(current_page==0)
+		current_page=num_of_pages-1;
+	else
+		current_page--;
+	auto_select(false);
+}
+
+
+
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+void
+initialize() {
+	std::cout << "Start!" << std::endl;
+	print_ez_template();
+	pros::delay(500);
 
-	pros::lcd::register_btn1_cb(on_center_button);
+	disable_all_tasks();
+
+	pros::lcd::initialize();
+	auto_select(false);
+	pros::lcd::register_btn0_cb(page_down);
+	pros::lcd::register_btn2_cb(page_up);
+	
+	if(!imu_calibrate()) {
+		pros::lcd::set_text(7, "IMU failed to calibrate!");
+	}
+
+	tare_sensors();
+	chassis_motor_init();
+
+	std::cout << "Auton" << std::endl;
+	Replay replay;
+	replay.start();
 }
+
+
 
 /**
  * Runs while the robot is in the disabled state of Field Management System or
  * the VEX Competition Switch, following either autonomous or opcontrol. When
  * the robot is enabled, this task will exit.
  */
-void disabled() {}
+void
+disabled() {
+	disable_all_tasks();
+}
+
+
 
 /**
  * Runs after initialize(), and before autonomous when connected to the Field
@@ -45,7 +140,12 @@ void disabled() {}
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-void competition_initialize() {}
+void
+competition_initialize() {
+	disable_all_tasks();
+}
+
+
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -58,7 +158,17 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void
+autonomous() {
+	tare_gyro();
+	reset_drive_sensor();
+	set_drive_brake(MOTOR_BRAKE_HOLD);
+	drive_pid.resume();
+
+	auto_select(true);
+}
+
+
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -73,20 +183,25 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+void
+opcontrol() { 
+	//Record rc; 
+	//rc.init();
+	drive_pid.suspend();
+	reset_drive_sensor();
+	set_drive_brake(MOTOR_BRAKE_COAST); // This is preference to what you like to drive on
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+		//rc.record(master);
+		chassis_joystick_control();
 
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
+		conveyor_control();
+
+		mogo_control();
+		tilter_control();
+
+		pros::delay(DELAY_TIME);
 	}
+
+	//rc.stopRecord();
 }
